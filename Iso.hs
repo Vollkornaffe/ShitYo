c76e1dccf8a240887782f06d4c985f8d4ab51481
Vollkorn <larshelge.scheel@gmail.com>
2014-08-30 03:36:30 +0200

    some ordering function for Ids

--- Iso.hs
+++ Iso.hs
@@ -1,15 +1,38 @@
 import UndirectedGraph
+import Data.List
 import Data.Char
 
-type Id = [Char]
+data Id = Id [Char] deriving (Eq,Ord,Show)
 data C = C Id GM deriving Show
 
 --lableV :: G -> [C]
 
 splitOnDeg :: G -> C -> [C]
-splitOnDeg g (C id gm) = filter (\(C _ z) -> z /= [])
-                         [(C ((if (allK k == gm)
-                               then 'r'
-                               else intToDigit k):id) (allK k))
-                         | k <- [0..maxDeg g gm]]
+splitOnDeg g (C (Id id) gm) = filter (\(C _ z) -> z /= [])
+                         [(C (Id ((if (allK k == gm)
+                                    then 'r'
+                                    else intToDigit k)
+                                   :id))
+                             (allK k)) | k <- [0..maxDeg g gm]]
     where allK k' = [x | x <- gm,k'== (degree g gm x)]
+
+sortId :: [Id] -> [Id]
+sortId [] = []
+sortId ids = (Id nextMax) : (sortId (delete (Id nextMax) ids))
+    where nextMax = mirror (foldl
+                                (\acu (Id xs) -> maxId (mirror xs []) acu) 
+                                 "#"
+                                 ids) []
+
+mirror :: [Char] -> [Char] -> [Char]
+mirror [] ys = ys
+mirror (x:xs) (ys) = (mirror xs (x:ys))
+
+maxId :: [Char] -> [Char] -> [Char]
+maxId [] ys = ys
+maxId xs [] = xs
+maxId (x:xs) (y:ys) = case (compare x y) of
+                         EQ -> x:(maxId xs ys)
+                         GT -> (x:xs)
+                         LT -> (y:ys)
+
